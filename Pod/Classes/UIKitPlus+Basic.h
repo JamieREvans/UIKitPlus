6199b7d8da231c54f0d9f2f86979a0da47785547
Jamie Evans <jevans@kineticcafe.com>
2015-04-01 15:58:37 -0400

    Added general import headers

---
+++ Pod/Classes/UIKitPlus+Basic.h
@@ -0,0 +1,30 @@
+//
+//  UIKitPlus+Basic.h
+//  Pods
+//
+//  Created by Jamie Evans on 2015-04-01.
+//  Copyright (c) 2015 Jamie Riley Evans. All rights reserved.
+//
+
+#ifndef Pods_UIKitPlus_Basic_h
+#define Pods_UIKitPlus_Basic_h
+
+#import "UIView+Framing.h"
+#import "NSDictionary+Attributes.h"
+#import "NSString+Sizing.h"
+#import "UIColor+Allocation.h"
+#import "UIImage+Network.h"
+#import "UIImage+Framing.h"
+#import "UIView+Alignment.h"
+#import "UIView+Allocation.h"
+#import "UIView+Subviews.h"
+#import "UIApplication+Navigation.h"
+#import "UIImageView+Allocation.h"
+#import "UIImageView+Network.h"
+#import "UIImageView+Rendering.h"
+#import "UILabel+Allocation.h"
+#import "UILabel+Framing.h"
+#import "UIButton+Allocation.h"
+#import "UIButton+Touches.h"
+
+#endif
---
+++ Pod/Classes/UIKitPlus.h
@@ -0,0 +1,15 @@
+//
+//  UIKitPlus.h
+//  Pods
+//
+//  Created by Jamie Evans on 2015-04-01.
+//  Copyright (c) 2015 Jamie Riley Evans. All rights reserved.
+//
+
+#ifndef Pods_UIKitPlus_h
+#define Pods_UIKitPlus_h
+
+#import "UIKitPlus+Basic.h"
+#import "UIKitPlus+Extra.h"
+
+#endif
